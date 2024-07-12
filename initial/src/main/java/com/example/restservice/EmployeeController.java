691562d611acbaa54d7e8b4ce0f07d43fde6e9f3
bhumika-aga <bhumika.aga@gmail.com>
2024-07-12 19:05:15 +0530

    Task 2 Updated

--- initial/src/main/java/com/example/restservice/EmployeeController.java
+++ initial/src/main/java/com/example/restservice/EmployeeController.java
@@ -1,14 +1,15 @@
 package com.example.restservice;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class EmployeeController {
@@ -22,13 +23,16 @@ public class EmployeeController {
     }
 
     @PostMapping("/employees")
-    public ResponseEntity<String> addEmployee(@RequestBody Employee employee) {
+    public ResponseEntity<Object> addEmployee(@RequestBody Employee employee) {
         Integer employeeId = employees.getAllEmployees().getEmployeeList().size() + 1;
 
         employee.setEmployee_id(employeeId.toString());
 
         employees.addEmployee(employee);
 
-        return new ResponseEntity<String>(HttpStatus.CREATED);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(employee.getEmployee_id()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
